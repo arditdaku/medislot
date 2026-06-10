1a2848fa625933e505f44c6a72c9d8f8a6c1fdde
drenxhyliqi <xhyliqiidren@gmail.com>
2026-06-10 12:17:59 +0200

    added providers to __init__ for migration

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.user import User
 from app.models.patient import Patient
+from app.models.provider import Provider
 
-__all__ = ["User", "Patient"]
+__all__ = ["User", "Patient", "Provider"]
