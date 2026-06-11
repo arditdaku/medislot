fd290f5d816287f721f5779dc606c166adafd06b
BlendSejdiu <119117327+BlendSejdiu@users.noreply.github.com>
2026-06-11 11:18:08 +0200

    Slot schema added

---
+++ backend/app/schemas/slot.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from uuid import UUID
+from pydantic import BaseModel, ConfigDict
+from app.models.appointment_slot import SlotStatus
+
+
+class SlotGenerate(BaseModel):
+    provider_id: int
+    start_time: datetime
+    end_time: datetime
+
+
+class SlotResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: UUID
+    provider_id: int
+    start_time: datetime
+    end_time: datetime
+    status: SlotStatus
